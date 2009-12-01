56cebea8de8efec0d556f6ccef57ab8548c9989b
Erik Michaels-Ober <sferik@gmail.com>
2009-12-01 11:39:19 -0800

    Version bump to 0.7.1

--- lib/merb-admin.rb
+++ lib/merb-admin.rb
@@ -24,7 +24,7 @@ if defined?(Merb::Plugins)
 
     # Slice metadata
     self.description = "MerbAdmin is a Merb plugin that provides an easy-to-use interface for managing your data."
-    self.version = "0.7.0"
+    self.version = "0.7.1"
     self.author = "Erik Michaels-Ober"
 
     # Stub classes loaded hook - runs before LoadClasses BootLoader
